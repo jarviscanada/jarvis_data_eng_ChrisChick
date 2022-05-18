7610c9519d95b68559a543b8c2c7a6cfa8e2ccfb
Chris Chick <chrisn8r@gmail.com>
2022-05-18 16:27:10 +0000

    PSQL startup script

---
+++ linux_sql/scripts/PSQLStartup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#get latest postgres
+docker pull postgres
+
+#Create new volume
+docker volume create pgdata
+
+#set password as 'postgres'
+export PGPASSWORD='postgres'
+
+#create container using psql image with name=jrvs-psql
+docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+
+#start container
+docker container start jrvs-psql
+
+#Install psql CLI client
+sudo yum install -y postgresql
